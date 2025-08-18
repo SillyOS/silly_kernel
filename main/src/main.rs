28868a2429311828f9e376bd05e2151d3d019e2b
temidaradev <ahmet.affan.ebcioglu@outlook.com>
2025-08-19 01:12:36 +0300

    broken macro rules (panic info prints in new line)

--- main/src/main.rs
+++ main/src/main.rs
@@ -2,16 +2,18 @@
 #![no_main]
 
 use core::panic::PanicInfo;
-use vga_buffer::*;
+use vga_buffer::println;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
+    panic!("Some panic message");
 
     loop {}
 }
