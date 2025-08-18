dcf2c881229f0f836b2ae85303c975b2c44a78d5
temidaradev <ahmet.affan.ebcioglu@outlook.com>
2025-08-19 00:41:58 +0300

    print macro

--- main/src/main.rs
+++ main/src/main.rs
@@ -4,12 +4,6 @@
 use core::panic::PanicInfo;
 use vga_buffer::*;
 
-#[macro_export]
-macro_rules! println {
-    () => (print!("\n"));
-    ($($arg:tt)*) => (print!("{}\n", format_args!($($arg)*)));
-}
-
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
@@ -17,15 +11,7 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    use core::fmt::Write;
-    vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
-    write!(
-        vga_buffer::WRITER.lock(),
-        ", some numbers: {} {}",
-        42,
-        1.337
-    )
-    .unwrap();
+    println!("Hello World{}", "!");
 
     loop {}
 }
