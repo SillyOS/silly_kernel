333860a32c5e6bb30d8fadfa96ada669933f989b
temidaradev <ahmet.affan.ebcioglu@outlook.com>
2025-09-02 15:07:45 +0300

    testing testing testing

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,21 @@ mod serial;
 mod vga_buffer;
 use core::panic::PanicInfo;
 
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
+}
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u32)]
 pub enum QemuExitCode {
@@ -51,18 +66,15 @@ pub extern "C" fn _start() -> ! {
 }
 
 #[cfg(test)]
-pub fn test_runner(tests: &[&dyn Fn()]) {
+pub fn test_runner(tests: &[&dyn Testable]) {
+    // new
     serial_println!("Running {} tests", tests.len());
     for test in tests {
-        test();
+        test.run(); // new
     }
-
     exit_qemu(QemuExitCode::Success);
 }
-
 #[test_case]
 fn trivial_assertion() {
-    serial_print!("trivial assertion... ");
     assert_eq!(1, 1);
-    serial_println!("[ok]");
 }
