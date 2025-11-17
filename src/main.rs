ec5e94002792277557138379f349e1db9fbc6ad0
temidaradev <ahmet.affan.ebcioglu@outlook.com>
2025-11-18 01:43:31 +0300

    test

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,9 @@ pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
 
     silly_os::init();
-
-    x86_64::instructions::interrupts::int3();
+    unsafe {
+        *(0xdeadbeef as *mut u64) = 42;
+    };
 
     #[cfg(test)]
     test_main();
